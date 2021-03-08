72aa3aeee9339ddc39b8b20bc7cae589622ef0cc
JasperSlimmens <jasper@slimmens.nl>
2021-03-08 14:47:12 +0100

    BDP WBTC strategy

---
+++ contracts/strategies/BDP/BDPStrategyMainnet_WBTC.sol
@@ -0,0 +1,31 @@
+pragma solidity 0.5.16;
+
+import "../../base/sushi-base/MasterChefStrategy.sol";
+
+contract BDPStrategyMainnet_WBTC is MasterChefStrategy {
+
+  address public wbtc_unused; // just a differentiator for the bytecode
+
+  constructor() public {}
+
+  function initializeStrategy(
+    address _storage,
+    address _vault
+  ) public initializer {
+    address underlying = address(0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599);
+    address weth = address(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
+    address bdp = address(0xf3dcbc6D72a4E1892f7917b7C43b74131Df8480e);
+    MasterChefStrategy.initializeStrategy(
+      _storage,
+      underlying,
+      _vault,
+      address(0x0De845955E2bF089012F682fE9bC81dD5f11B372), // master chef contract
+      bdp,
+      0,  // Pool id
+      false, // is LP asset
+      true // false = use Sushiswap for liquidating
+    );
+    // sushi is token0, weth is token1
+    uniswapRoutes[underlying] = [bdp, weth, underlying];
+  }
+}
