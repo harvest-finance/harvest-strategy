9ff761f37e211ec9f8588e2e1745c0030d389c35
DanielZlotin <zlotindaniel@gmail.com>
2021-04-18 20:36:54 +0300

    productionize

--- test/nexus/nexus-sushi-weth.js
+++ test/nexus/nexus-sushi-weth.js
@@ -9,7 +9,7 @@ const NexusLP = artifacts.require("contracts/strategies/nexus/interface/INexusLP
 
 const NEXUS_ADDRESS = "0x82DE6a95b5fe5CB38466686Ee09D4dC74C9b4A1a";
 
-describe("LiquidityNexus SushiSwap: ETH", () => {
+describe("LiquidityNexus SushiSwap: WETH", () => {
   // parties in the protocol
   const governance = "0xf00dD244228F51547f0563e60bCa65a30FBF5f7f"; // Harvest.finance deployer
   let nexusOwner;
@@ -20,7 +20,16 @@ describe("LiquidityNexus SushiSwap: ETH", () => {
   let vault;
   let strategy;
 
+  // underlying
   let nexus;
+
+  // test parameters
+  const doHardWorkWaitHours = 12;
+  const avgBlockDurationSeconds = 13.2;
+  const blocksPerHour = (60 * 60) / avgBlockDurationSeconds;
+  const doHardWorkCount = 10;
+  const depositETH = bn(web3.utils.toWei("1000", "ether"));
+  let farmerStartBalanceETH;
 
   before(async function () {
     console.log("on block number", await web3.eth.getBlockNumber());
@@ -39,30 +48,28 @@ describe("LiquidityNexus SushiSwap: ETH", () => {
       strategyArtifact: Strategy,
       strategyArtifactIsUpgradable: true,
       governance: governance,
-      strategyArgs: [addresses.Storage, nexus.address, "vaultAddr"],
-      underlying: { address: nexus.address },
+      strategyArgs: [addresses.Storage, NEXUS_ADDRESS, "vaultAddr"],
+      underlying: { address: NEXUS_ADDRESS },
     });
 
     nexus.setGovernance(strategy.address, { from: nexusOwner });
 
     await prepareNexusWithUSDC(usdcWhale);
+
+    farmerStartBalanceETH = bn(await web3.eth.getBalance(farmer));
   });
 
   async function prepareNexusWithUSDC(usdcWhale) {
     const amount = "10000000000000"; // $10M
     const USDC = await IERC20.at("0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48");
     await USDC.transfer(nexusOwner, amount, { from: usdcWhale });
-    await USDC.approve(nexus.address, amount, { from: nexusOwner });
+    await USDC.approve(NEXUS_ADDRESS, amount, { from: nexusOwner });
     await nexus.depositCapital(amount, { from: nexusOwner });
   }
 
   it("Farmer should earn", async () => {
-    const depositETH = bn(web3.utils.toWei("1000", "ether"));
-
-    const farmerStartBalanceETH = bn(await web3.eth.getBalance(farmer));
-
-    Utils.assertBNGt(farmerStartBalanceETH, depositETH);
-    Utils.assertBNGt(await nexus.availableSpaceToDepositETH(), depositETH); // how much ETH can be deposited to be paired with USDC
+    const availableSpaceETH = await nexus.availableSpaceToDepositETH();  // how much ETH can be deposited to be paired with USDC
+    Utils.assertBNGt(availableSpaceETH, depositETH);
 
     console.log(`farmer enters LiquidityNexus with ${fmt(depositETH)} ETH`);
     await nexus.addLiquidityETH(farmer, deadline, { value: bn(depositETH), from: farmer }); // deposit accepts ETH or WETH
@@ -74,15 +81,10 @@ describe("LiquidityNexus SushiSwap: ETH", () => {
     await vault.deposit(farmerStartBalanceLP, { from: farmer });
 
     // Using half days is to simulate how we doHardwork in the real world
-    const doHardWorkWaitHours = 12;
-    const avgBlockDurationSeconds = 13.2;
-    const blocksPerHour = (60 * 60) / avgBlockDurationSeconds;
-    const iterations = 10;
-
     let oldSharePrice;
     let newSharePrice;
 
-    for (let i = 0; i < iterations; i++) {
+    for (let i = 0; i < doHardWorkCount; i++) {
       console.log("loop ", i);
       await Utils.advanceNBlock(blocksPerHour * doHardWorkWaitHours);
 
@@ -93,8 +95,8 @@ describe("LiquidityNexus SushiSwap: ETH", () => {
       Utils.assertBNEq(newSharePrice, oldSharePrice); // 1:1 ratio Nexus:Vault
       newSharePrice = newSharePrice.multipliedBy(await nexus.pricePerFullShare()).div(1e18); // NexusLP price increase
 
-      console.log("old shareprice: ", fmt(oldSharePrice));
-      console.log("new shareprice: ", fmt(newSharePrice));
+      console.log("old shareprice: ", oldSharePrice.toFixed());
+      console.log("new shareprice: ", newSharePrice.toFixed());
       console.log("growth: ", newSharePrice.toFixed() / oldSharePrice.toFixed());
     }
 
@@ -108,8 +110,15 @@ describe("LiquidityNexus SushiSwap: ETH", () => {
     Utils.assertBNEq(farmerEndBalanceLP, farmerStartBalanceLP); // 1:1 ratio Nexus:Vault
 
     console.log("farmer exits LiquidityNexus...");
-    await nexus.removeAllLiquidityETH(farmer, deadline, { from: farmer });
+    await nexus.removeAllLiquidityETH(farmer, deadline, { from: farmer }); // able to withdraw ETH or WETH
+    await printAPY();
 
+    console.log("earned!");
+
+    await strategy.withdrawAllToVault({ from: governance }); // making sure can withdraw all for a next switch
+  });
+
+  async function printAPY() {
     const farmerEndBalanceETH = bn(await web3.eth.getBalance(farmer));
     Utils.assertBNGt(farmerEndBalanceETH, farmerStartBalanceETH);
     console.log("start ETH balance", fmt(farmerStartBalanceETH));
@@ -119,7 +128,7 @@ describe("LiquidityNexus SushiSwap: ETH", () => {
     const profit = farmerEndBalanceETH.minus(farmerStartBalanceETH);
     console.log("profit", fmt(profit), "ETH");
 
-    const testDuration = doHardWorkWaitHours * iterations;
+    const testDuration = doHardWorkWaitHours * doHardWorkCount;
     console.log("Test duration", testDuration, "hours");
 
     const profitPercent = profit / depositETH.toFixed();
@@ -133,11 +142,7 @@ describe("LiquidityNexus SushiSwap: ETH", () => {
 
     const APY = Math.pow(1 + dailyYield / 2, 365 * 2) - 1; // compounding twice a day
     console.log("APY", APY * 100, "%");
-
-    console.log("earned!");
-
-    await strategy.withdrawAllToVault({ from: governance }); // making sure can withdraw all for a next switch
-  });
+  }
 });
 
 function bn(n) {
