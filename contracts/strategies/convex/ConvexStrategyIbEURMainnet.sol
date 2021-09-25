09d68fc8e61f50a901250c49b890695af81bb052
bergben <bergben@tutanota.com>
2021-09-25 19:21:37 +0200

    feat(convex): add strategy for fixed forex EUR (ibEUR)

---
+++ contracts/strategies/convex/ConvexStrategyIbEURMainnet.sol
@@ -0,0 +1,41 @@
+pragma solidity 0.5.16;
+
+import "./base/ConvexStrategyUL.sol";
+
+contract ConvexStrategyIbEURMainnet is ConvexStrategyUL {
+
+  address public ibeur_unused; // just a differentiator for the bytecode
+
+  constructor() public {}
+
+  function initializeStrategy(
+    address _storage,
+    address _vault
+  ) public initializer {
+    address underlying = address(0x19b080FE1ffA0553469D20Ca36219F17Fcf03859);
+    address rewardPool = address(0xCd0559ADb6fAa2fc83aB21Cf4497c3b9b45bB29f);
+    address crv = address(0xD533a949740bb3306d119CC777fa900bA034cd52);
+    address cvx = address(0x4e3FBD56CD56c3e72c1403e103b45Db9da5B9D2B);
+    address ibeur = address(0x96E61422b6A9bA0e068B6c5ADd4fFaBC6a4aae27);
+    bytes32 sushiDex = bytes32(0xcb2d20206d906069351c89a2cb7cdbd96c71998717cd5a82e724d955b654f67a);
+    ConvexStrategyUL.initializeBaseStrategy(
+      _storage,
+      underlying,
+      _vault,
+      rewardPool, //rewardPool
+      45,  // Pool id
+      ibeur,
+      0, //depositArrayPosition
+      underlying,
+      2, //nTokens
+      false //metaPool
+    );
+    rewardTokens = [crv, cvx];
+    storedLiquidationPaths[crv][weth] = [crv, weth];
+    storedLiquidationDexes[crv][weth] = [sushiDex];
+    storedLiquidationPaths[cvx][weth] = [cvx, weth];
+    storedLiquidationDexes[cvx][weth] = [sushiDex];
+    storedLiquidationPaths[weth][ibeur] = [weth, ibeur];
+    storedLiquidationDexes[weth][ibeur] = [sushiDex];
+  }
+}
---
+++ test/convex/ibeur.js
@@ -0,0 +1,137 @@
+// Utilities
+const Utils = require("../utilities/Utils.js");
+const { impersonates, setupCoreProtocol, depositVault } = require("../utilities/hh-utils.js");
+
+const BigNumber = require("bignumber.js");
+const IERC20 = artifacts.require("@openzeppelin/contracts/token/ERC20/IERC20.sol:IERC20");
+const IBooster = artifacts.require("IBooster");
+
+const Strategy = artifacts.require("ConvexStrategyIbEURMainnet");
+
+//This test was developed at blockNumber 13295726
+
+// Vanilla Mocha test. Increased compatibility with tools that integrate Mocha.
+describe("Mainnet Convex IbEUR", function() {
+  let accounts;
+
+  // external contracts
+  let underlying;
+
+  // external setup
+  let underlyingWhale = "0x3Ee505bA316879d246a8fD2b3d7eE63b51B44FAB";
+  let crv = "0xD533a949740bb3306d119CC777fa900bA034cd52";
+  let cvx = "0x4e3FBD56CD56c3e72c1403e103b45Db9da5B9D2B";
+  let ibeur = "0x96E61422b6A9bA0e068B6c5ADd4fFaBC6a4aae27";
+  let weth = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
+  let hodlVault = "0xF49440C1F012d041802b25A73e5B0B9166a75c02";
+  let booster;
+
+  // parties in the protocol
+  let governance;
+  let farmer1;
+
+  // numbers used in tests
+  let farmerBalance;
+
+  // Core protocol contracts
+  let controller;
+  let vault;
+  let strategy;
+
+  async function setupExternalContracts() {
+    underlying = await IERC20.at("0x19b080FE1ffA0553469D20Ca36219F17Fcf03859");
+    console.log("Fetching Underlying at: ", underlying.address);
+  }
+
+  async function setupBalance(){
+    let etherGiver = accounts[9];
+    // Give whale some ether to make sure the following actions are good
+    await web3.eth.sendTransaction({ from: etherGiver, to: underlyingWhale, value: 1e18});
+
+    farmerBalance = await underlying.balanceOf(underlyingWhale);
+    await underlying.transfer(farmer1, farmerBalance, { from: underlyingWhale });
+  }
+
+  before(async function() {
+    governance = "0xf00dD244228F51547f0563e60bCa65a30FBF5f7f";
+    accounts = await web3.eth.getAccounts();
+
+    farmer1 = accounts[1];
+
+    // impersonate accounts
+    await impersonates([governance, underlyingWhale]);
+
+    await setupExternalContracts();
+    [controller, vault, strategy] = await setupCoreProtocol({
+      "strategyArtifact": Strategy,
+      "strategyArtifactIsUpgradable": true,
+      "underlying": underlying,
+      "governance": governance,
+      "liquidation": [{"sushi": [cvx, weth]},
+                      {"sushi": [crv, weth]},
+                      {"sushi": [weth, ibeur]}],
+
+    });
+
+    await strategy.setSellFloor(0, {from:governance});
+
+    booster = await IBooster.at("0xF403C135812408BFbE8713b5A23a04b3D48AAE31");
+
+    // whale send underlying to farmers
+    await setupBalance();
+  });
+
+  describe("Happy path", function() {
+    it("Farmer should earn money", async function() {
+      let farmerOldBalance = new BigNumber(await underlying.balanceOf(farmer1));
+      await depositVault(farmer1, underlying, vault, farmerBalance);
+      let fTokenBalance = await vault.balanceOf(farmer1);
+      let cvxToken = await IERC20.at("0x4e3FBD56CD56c3e72c1403e103b45Db9da5B9D2B");
+      let hodlOldBalance = new BigNumber(await cvxToken.balanceOf(hodlVault));
+
+      // Using half days is to simulate how we doHardwork in the real world
+      let hours = 10;
+      let blocksPerHour = 2400;
+      let oldSharePrice;
+      let newSharePrice;
+      for (let i = 0; i < hours; i++) {
+        console.log("loop ", i);
+
+        await booster.earmarkRewards(await strategy.poolId());
+
+        oldSharePrice = new BigNumber(await vault.getPricePerFullShare());
+        await controller.doHardWork(vault.address, { from: governance });
+        newSharePrice = new BigNumber(await vault.getPricePerFullShare());
+
+        console.log("old shareprice: ", oldSharePrice.toFixed());
+        console.log("new shareprice: ", newSharePrice.toFixed());
+        console.log("growth: ", newSharePrice.toFixed() / oldSharePrice.toFixed());
+
+        apr = (newSharePrice.toFixed()/oldSharePrice.toFixed()-1)*(24/(blocksPerHour/272))*365;
+        apy = ((newSharePrice.toFixed()/oldSharePrice.toFixed()-1)*(24/(blocksPerHour/272))+1)**365;
+
+        console.log("instant APR:", apr*100, "%");
+        console.log("instant APY:", (apy-1)*100, "%");
+
+        await Utils.advanceNBlock(blocksPerHour);
+      }
+      await vault.withdraw(fTokenBalance, { from: farmer1 });
+      let farmerNewBalance = new BigNumber(await underlying.balanceOf(farmer1));
+      Utils.assertBNGt(farmerNewBalance, farmerOldBalance);
+
+      let hodlNewBalance = new BigNumber(await cvxToken.balanceOf(hodlVault));
+      console.log("CVX before", hodlOldBalance.toFixed());
+      console.log("CVX after ", hodlNewBalance.toFixed());
+      Utils.assertBNGt(hodlNewBalance, hodlOldBalance);
+
+      apr = (farmerNewBalance.toFixed()/farmerOldBalance.toFixed()-1)*(24/(blocksPerHour*hours/272))*365;
+      apy = ((farmerNewBalance.toFixed()/farmerOldBalance.toFixed()-1)*(24/(blocksPerHour*hours/272))+1)**365;
+
+      console.log("earned!");
+      console.log("Overall APR:", apr*100, "%");
+      console.log("Overall APY:", (apy-1)*100, "%");
+
+      await strategy.withdrawAllToVault({ from: governance }); // making sure can withdraw all for a next switch
+    });
+  });
+});
