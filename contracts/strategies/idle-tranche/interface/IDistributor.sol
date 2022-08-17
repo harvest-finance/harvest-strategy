7606bdbbd5eb11d2af7211b801fcced28406bfbb
tommychan4241 <tommychan4241@gmail.com>
2022-08-18 01:18:12 +1000

    Idle Tranche strategy contract and unit test

---
+++ contracts/strategies/idle-tranche/interface/IDistributor.sol
@@ -0,0 +1,5 @@
+pragma solidity 0.5.16;
+
+contract IDistributor {
+    function distribute(address gauge_addr) external;
+}
---
+++ contracts/strategies/idle-tranche/interface/IWstETH.sol
@@ -0,0 +1,7 @@
+pragma solidity 0.5.16;
+
+contract IWstETH {
+    function wrap(uint256 _stETHAmount) external returns (uint256);
+    function unwrap(uint256 _wstETHAmount) external returns (uint256);
+    function getWstETHByStETH(uint256 _stETHAmount) external view returns (uint256);
+}
